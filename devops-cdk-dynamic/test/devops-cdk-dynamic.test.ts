f7b65f5930c90106af7f3d4c2ccab60293667e66
oladayo <akinlonudayo@yahoo.com>
2025-10-28 21:18:06 -0400

    CDK Dynamic resource

---
+++ devops-cdk-dynamic/test/devops-cdk-dynamic.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevopsCdkDynamic from '../lib/devops-cdk-dynamic-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/devops-cdk-dynamic-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevopsCdkDynamic.DevopsCdkDynamicStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
